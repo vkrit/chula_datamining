6e31c2dcdf0b6f50c36b0a734ccbc0855a73aa52
Veerasak Kritsanapraphan <veerasak.kr@172.18.40.66>
2015-11-13 17:33:16 +0700

    fix error

--- Association_Rule.R
+++ Association_Rule.R
@@ -1,11 +1,13 @@
 install.packages('seriation')
 install.packages('arules')
 install.packages('arulesViz')
+install.packages('iplots')
 
 # Load the libraries
 library(arules)
 library(arulesViz)
 library(datasets)
+library(iplots)
 
 # Load the data set
 data(Groceries)
@@ -20,6 +22,11 @@ options(digits=2)
 inspect(rules[1:5])
 summary(rules)
 
+inspect(head(sort(rules, by="lift"),10));
+
+plot(rules);
+plot(rules, method="grouped")
+
 #sort rules
 rules<-sort(rules, by="confidence", decreasing=TRUE)
 inspect(rules[1:5])
@@ -28,7 +35,12 @@ inspect(rules[1:5])
 rules <- apriori(Groceries, parameter = list(supp = 0.001, conf = 0.8,maxlen=3))
 inspect(rules[1:5])
 summary(rules)
+?plot
+plot(rules, method="graph")
+plot(rules, method="paracoord")
 
+
+?is.subset
 # Rules pruned
 subset.matrix <- is.subset(rules, rules)
 subset.matrix[lower.tri(subset.matrix, diag=T)] <- NA
@@ -36,6 +48,7 @@ redundant <- colSums(subset.matrix, na.rm=T) >= 1
 rules.pruned <- rules[!redundant]
 rules<-rules.pruned
 summary(rules)
+plot(rules, method="grouped")
 
 # What are customers likely to buy after buying whole milk?
 rules<-apriori(data=Groceries, parameter=list(supp=0.001,conf = 0.08), 
--- Hierarchical-Clustering.R
+++ Hierarchical-Clustering.R
@@ -1,5 +1,9 @@
 set.seed(101)
-?sampl
+
+?dist
+?hclust
+?plot
+
 sampleiris <- iris[sample(1:150, 40),] # get samples from iris dataset
 # each observation has 4 variables, ie, they are interpreted as 4-D points
 distance   <- dist(sampleiris[,-5], method="euclidean") 
@@ -7,12 +11,12 @@ cluster    <- hclust(distance, method="average")
 plot(cluster, hang=-1, label=sampleiris$Species)
 
 ?par
-
+plot.new()
 par(mfrow=c(1,2))
 group.3 <- cutree(cluster, k = 3)  # prune the tree by 3 clusters
 table(group.3, sampleiris$Species) # compare with known classes
 
-plot(sampleiris[,c(1,2)], col=group.3, pch=19, cex=2.5, main="3 clusters")
-points(sampleiris[,c(1,2)], col=group.3, pch=19, cex=1)
-plot(sampleiris[,c(1,2)], col=sampleiris$Species, pch=19, cex=2.5, main="3 clusters")
-points(sampleiris[,c(1,2)], col=sampleiris$Species, pch=19, cex=1)
+plot(sampleiris[,c(1,2)], col=group.3, pch=19, cex=1, main="3 clusters")
+points(sampleiris[,c(1,2)], col=group.3, pch=9, cex=2)
+plot(sampleiris[,c(1,2)], col=sampleiris$Species, pch=1, cex=1, main="3 clusters")
+points(sampleiris[,c(1,2)], col=sampleiris$Species, pch=10, cex=2)
